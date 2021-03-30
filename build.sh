64afd2a1275a2b092be3e7d0f1f4642b62a8e94f
Artem V. Navrotskiy <bozaro@yandex.ru>
2021-03-30 12:17:40 +0300

    Add libvips patch for fix jpeg EOF issue

--- build.sh
+++ build.sh
@@ -1,16 +1,18 @@
-#!/bin/bash
+#!/bin/bash -ex
+
+DISTVERSION=20.04
+DISTRIBUTION=hirsute
 
 PACKAGE=vips
-DATE=`date --iso-8601=minutes`
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [ -z "${DISTRIBUTION}" ]; then
-	echo Define environment variable DISTRIBUTION "(e.g. focal)"
+	echo "Define environment variable DISTRIBUTION as backport source (e.g. groovy)"
 	exit 1
 fi
 
 if [ -z "${DISTVERSION}" ]; then
-	echo Define environment variable DISTVERSION "(e.g. 20.04)"
+	echo "Define environment variable DISTVERSION as backport target (e.g. 20.04)"
 	exit 1
 fi
 
@@ -23,44 +25,5 @@ echo Building base image:
 
 docker build -t build-$PACKAGE:$DISTVERSION -f $DIR/Dockerfile-$PACKAGE-$DISTVERSION $DIR || (echo build failed 1>&2 && exit 1)
 
-echo
-
-if [ ! -d ~/.buildvers ]; then
-	mkdir -p ~/.buildvers
-fi
-
-echo Checking newest version...
-NEWVERSION=`docker run --rm build-$PACKAGE:$DISTVERSION bash -c "apt-get -qq update && rmadison $PACKAGE | grep $DISTRIBUTION | sed 's/$PACKAGE . //;s/ . $DISTRIBUTION.*//;s/ //g;'"`
-
-if [ -f ~/.buildvers/$PACKAGE-$DISTRIBUTION ]; then
-	OLDVERSION=`cat ~/.buildvers/$PACKAGE-$DISTRIBUTION`
-	if [ "$OLDVERSION" = "$NEWVERSION" ]; then
-		echo Versions match, no need to update
-		exit
-	fi
-	echo Version changed from $OLDVERSION to $NEWVERSION, creating new package
-else
-	echo No old version found, creating new package for $NEWVERSION
-fi
-
-echo -n Press enter to continue or Ctrl-C to abort:' '
-read
-
-clean_gpg() {
-	echo Cleaning GPG keys...
-	rm -f /tmp/volume-$PACKAGE-$DISTRIBUTION/keys.asc
-	rm -rf /tmp/volume-$PACKAGE-$DISTRIBUTION
-}
-
-trap clean_gpg SIGINT SIGTERM EXIT
-
-mkdir -p /tmp/volume-$PACKAGE-$DISTRIBUTION
-gpg --export-secret-keys --armor >> /tmp/volume-$PACKAGE-$DISTRIBUTION/keys.asc || (echo "Couldn't write GPG keys" && exit 1)
-
-docker run --rm -i -e DATE=$DATE -v /tmp/volume-$PACKAGE-$DISTRIBUTION:/volume build-$PACKAGE:$DISTVERSION bash /within_container-$PACKAGE.sh
-clean_gpg
-
-echo -n Press enter to mark $NEWVERSION as successfully uploaded or Ctrl-C to abort:' '
-read
-
-echo $NEWVERSION > ~/.buildvers/$PACKAGE-$DISTRIBUTION
+mkdir -p packages/$DISTVERSION
+docker run --rm -ti -e SOURCEDIST=$DISTRIBUTION -v $(pwd)/packages/$DISTVERSION:/packages build-$PACKAGE:$DISTVERSION /within_container-$PACKAGE.sh
--- within_container-vips.sh
+++ within_container-vips.sh
@@ -1,19 +1,26 @@
-#!/bin/sh
+#!/bin/bash -ex
 
-echo Building package $PACKAGE for distribution $DISTRIBUTION to PPA $PPANAME on date $DATE
+echo Building package $PACKAGE for distribution $DISTRIBUTION to PPA $PPANAME
 
-gpg --import /volume/keys.asc || exit 1
-mkdir -p /tmp/build-$PACKAGE-$DISTRIBUTION-$DATE && cd /tmp/build-$PACKAGE-$DISTRIBUTION-$DATE
-apt-get -q -y source $PACKAGE || exit 1
+echo deb-src http://ru.archive.ubuntu.com/ubuntu/ $SOURCEDIST universe > /etc/apt/sources.list.d/ubuntu-backport.list
+apt update
+
+mkdir -p /tmp/build-$PACKAGE-$DISTRIBUTION && cd /tmp/build-$PACKAGE-$DISTRIBUTION
+apt-get -q -y source $PACKAGE
 
 SRCDIR=`find . -mindepth 1 -maxdepth 1 -type d`
 echo Source dir is "$SRCDIR"
-cd "$SRCDIR/debian" || exit 1
+
+cd "$SRCDIR/debian"
+
+cp /patches/*.patch patches/
+ls /patches >> patches/series
 
 cat control | sed 's/^Build-Depends: /Build-Depends: libheif-dev, /' > control.new
 mv control.new control
 
-dch --local tonimelisma --distribution $DISTRIBUTION 'Add HEIF support' || exit 1
-debuild -S -sd || exit 1
-cd ../..
-dput ppa:$PPANAME/ppa `find . -type f -name '*source.changes'`
+dch --local tonimelisma --distribution $DISTRIBUTION 'Add HEIF support'
+dch --local bozaro --distribution $DISTRIBUTION 'Signal error on EOF in jpegload more reliably'
+debuild -us -uc
+
+cp ../../*.deb /packages/
